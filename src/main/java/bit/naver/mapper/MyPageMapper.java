52aa53c99356662ea1be880bb211f6f6208e143e
yujung <148977734+yujung1339@users.noreply.github.com>
2024-06-25 16:04:09 +0900

    이력서 첨부 추가(업로드 및 다운로드)

--- src/main/java/bit/naver/mapper/MyPageMapper.java
+++ src/main/java/bit/naver/mapper/MyPageMapper.java
@@ -1,6 +1,7 @@
 package bit.naver.mapper;
 
 import bit.naver.entity.LikeReferencesEntity;
+import bit.naver.entity.ResumesEntity;
 import bit.naver.entity.StudyReferencesEntity;
 import bit.naver.entity.Users;
 import org.apache.ibatis.annotations.Mapper;
@@ -12,6 +13,8 @@ import java.util.List;
 public interface MyPageMapper {
     List<StudyReferencesEntity> getStudyReferencesList(StudyReferencesEntity entity);
 
+    List<ResumesEntity> getResumesList(ResumesEntity entity);
+
     List<StudyReferencesEntity> getLikePostList(@Param("userIdx") String userIdx, @Param("searchKeyword") String searchKeyword, @Param("searchOption") String searchOption, @Param("limits")String limits);
 
     int insertLike(LikeReferencesEntity entity);
@@ -22,4 +25,9 @@ public interface MyPageMapper {
 
     int minusLike(LikeReferencesEntity entity);
 
+    ResumesEntity getMyPageById(Long resumeIdx);
+
+    int uploadResume(ResumesEntity entity);
+
+    int deleteResume(String resumeIdx);
 }
