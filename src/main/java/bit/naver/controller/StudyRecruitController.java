04f7558e8eadf7b8226460b41a203f711a99e72c
jeon <wjadbwjd51@gamail.com>
2024-06-26 12:58:07 +0900

    메인 페이지에 스터디 모집 좋아요 기능 구현 스터디 모집 페이지에 좋아요 및 신고 버튼 기능 구현

--- src/main/java/bit/naver/controller/StudyRecruitController.java
+++ src/main/java/bit/naver/controller/StudyRecruitController.java
@@ -1,16 +1,16 @@
 package bit.naver.controller;
 
+import bit.naver.entity.LikeStudyEntity;
 import bit.naver.entity.StudyGroup;
 import bit.naver.entity.StudyMembers;
 import bit.naver.entity.Users;
 import bit.naver.mapper.StudyRecruitMapper;
+import bit.naver.mapper.UsersMapper;
 import bit.naver.service.StudyRecruitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.security.Principal;
